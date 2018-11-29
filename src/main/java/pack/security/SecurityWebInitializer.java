b24463d3827e5ad6747622539100aa853ebbdec0
mateusz.wojdat <mateusz.wojdat@student.wat.edu.pl>
2018-11-29 22:17:38 +0100

    users z pliku

--- src/main/java/pack/security/SecurityWebInitializer.java
+++
@@ -1,5 +0,0 @@
-//package pack.security;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {}

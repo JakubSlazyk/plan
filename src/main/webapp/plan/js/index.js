6139db5a78541a2823272b9c87491d1533f449e5
Slazyk <jslazyk@mac-jslazyk.local>
2018-12-01 23:02:56 +0100

    Add searching for group

--- src/main/webapp/plan/js/index.js
+++ src/main/webapp/plan/js/index.js
@@ -1,27 +1,44 @@
 function getPlan() {
-    let url = 'http://localhost:8080/plan/plann';
+    let url = '../plann';
     fetch(url, {
         cache: "no-cache",
         pragma: "no-cache",
         "cache-control": "no-cache"
     }).then((response) => response.json())
         .then((planJson) => {
-            console.log(planJson.events);
-            $('#calendar').fullCalendar({
-                theme: true,
-                businessHours: true,
-                editable: true,
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay'
-                },
-                events: planJson.events,
-                eventClick: openModal
+            let eventy = planJson.events;
+            let grupa = getAllUrlParams(window.location.href);
+            fetchLoggedUserData().then((userInfo) => {
+                let id = userInfo.id;
+                fetchOsoby().then(osoby => {
+                    if (Object.keys(grupa).length < 1) {
+                        let osobyArray = osoby.osoby;
+                        const osoba = osobyArray.filter(osoba => osoba.id === id)[0];
+                        if (osoba.grupa != "")
+                            eventy = eventy.filter((event) => event.grupa == osoba.grupa);
+                        else
+                            eventy = eventy.filter((event) => event.idWykladowcy == osoba.id);
+                    } else {
+                        eventy = eventy.filter((event) => event.grupa == grupa.grupa);
+                    }
+                    $('#calendar').fullCalendar({
+                        theme: true,
+                        businessHours: true,
+                        editable: true,
+                        header: {
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'month,agendaWeek,agendaDay'
+                        },
+                        events: eventy,
+                        eventClick: openModal
+                    });
+                });
             });
+
         });
 }
-function updateNavbar() {
+function updateNavbar(osoba) {
     fetchLoggedUserData().then((userInfo) => {
         if (userInfo.role === "WYKLADOWCA") {
             document.getElementById('prosbyLink').style.display = "inline-block";
@@ -36,6 +53,7 @@ function updateNavbar() {
             const userName = `${osoba.imie} ${osoba.nazwisko}`;
             document.getElementById('user').textContent = userName;
             document.getElementById('grupa').textContent = grupa;
+
         });
 
     });
@@ -208,4 +226,74 @@ function fetchOsoby() {
     let url = '../osoby';
     return fetch(url).
         then((response) => response.json());
+}
+
+function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+    // we'll store the parameters here
+    var obj = {};
+
+    // if query string exists
+    if (queryString) {
+
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
+
+        // split our query string into its component parts
+        var arr = queryString.split('&');
+
+        for (var i = 0; i < arr.length; i++) {
+            // separate the keys and the values
+            var a = arr[i].split('=');
+
+            // set parameter name and value (use 'true' if empty)
+            var paramName = a[0];
+            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+            // (optional) keep case consistent
+            paramName = paramName.toLowerCase();
+            if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+            // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+            if (paramName.match(/\[(\d+)?\]$/)) {
+
+                // create key if it doesn't exist
+                var key = paramName.replace(/\[(\d+)?\]/, '');
+                if (!obj[key]) obj[key] = [];
+
+                // if it's an indexed array e.g. colors[2]
+                if (paramName.match(/\[\d+\]$/)) {
+                    // get the index value and add the entry at the appropriate position
+                    var index = /\[(\d+)\]/.exec(paramName)[1];
+                    obj[key][index] = paramValue;
+                } else {
+                    // otherwise add the value to the end of the array
+                    obj[key].push(paramValue);
+                }
+            } else {
+                // we're dealing with a string
+                if (!obj[paramName]) {
+                    // if it doesn't exist, create property
+                    obj[paramName] = paramValue;
+                } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                    // if property does exist and it's a string, convert it to an array
+                    obj[paramName] = [obj[paramName]];
+                    obj[paramName].push(paramValue);
+                } else {
+                    // otherwise add the property
+                    obj[paramName].push(paramValue);
+                }
+            }
+        }
+    }
+
+    return obj;
+}
+
+function searchForGroup() {
+    const groupToSearch = document.getElementById('grupaSearch').value;
+    window.location.replace(window.location.href.split('?')[0] + `?grupa=${groupToSearch}`);
 }
